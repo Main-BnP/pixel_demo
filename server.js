617bc9df14f2dc2f0556fc6040a79805ad59da38
Akshay Tyagi <akshay.tyagi@touchcast.com>
2023-11-03 15:18:19 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+
+app.get("/", (request, response) => {
+    res.render('index');
+    next();
+});
+
+
+app.listen(3000, () => {
+    console.log("Listen on the port 3000...");
+});
